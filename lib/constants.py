bb68a3c3c1126b637db1bcedc5482a1d0808a14f
Markiian Lukashiv <markiian.lukashiv@globallogic.com>
2019-05-02 18:16:41 +0300

    Added another percent division and refactored code (added strategy function)

--- lib/constants.py
+++ lib/constants.py
@@ -15,3 +15,31 @@ XML_ATTRIBUTES = {
     'DECISIONS_COVERED': 'd_cov',
     'DECISIONS_TOTAL': 'd_total'
 }
+
+CUSTOM_XML_FIELDS = {
+    'FILES': 'files',
+    'PROBES': 'probes',
+    'FUNCTIONS': 'functions',
+    'INFO': 'info',
+
+}
+
+COLUMN_INDEXES = {
+    'name': 1,
+    'fn_cov': 2,
+    'fn_uncov': 3,
+    'fn_total': 4,
+    'cd_cov_d_cov': 5,
+    'cd_uncov_d_uncov': 6,
+    'cd_total': 7,
+    'd_total': 8
+}
+
+FIELDS_TO_PERCENTIZE = ['fn_cov', 'cd_cov_d_cov']
+
+EXCLUDE_FIELDS = ['d_cov', 'cd_cov', ]
+
+NAME_COLUMN = "A:A"
+FUNCTION_NAME_COLUMN = "B:B"
+UNCOVERED_FUNCTIONS_COLUMN = "I:I"
+UNCOVERED_DECISIONS_CONDITIONS_COLUMN = "F:F"
--- lib/xls.py
+++ lib/xls.py
@@ -2,7 +2,14 @@ import xlsxwriter
 import math
 import os
 from decimal import Decimal
-from .constants import XML_ATTRIBUTES, XML_FIELDS
+import lib.constants as constant
+
+
+CUSTOM_COLUMNS = [
+    'fn_uncov',
+    'cd_cov_d_cov',
+    'cd_uncov_d_uncov',
+]
 
 
 def build_xlsx_file(filename, data):
@@ -11,14 +18,18 @@ def build_xlsx_file(filename, data):
     overall(data, workbook)
 
     for raw_data_for_sheet in data:
-        sheet = workbook.add_worksheet(raw_data_for_sheet['folder']['name'])
+        sheet = workbook.add_worksheet(get_sheet_name(raw_data_for_sheet))
         fill_sheet(raw_data_for_sheet, sheet)
     workbook.close()
 
 
+def get_sheet_name(sheet_data):
+    return sheet_data[constant.XML_FIELDS.get('FOLDER')][constant.XML_FIELDS.get('NAME')]
+
+
 def fill_sheet(raw_data, sheet):
     sheet.write(0, 0, "file name")
-    sheet.write(0, 1, "function names")
+    # sheet.write(0, 1, "function names")
 
     filename_column_index = 0
     filename_column = "A:A"
@@ -29,13 +40,10 @@ def fill_sheet(raw_data, sheet):
     filename_row = 2
     func_row = filename_row + 1
 
-    example_func_for_stats = raw_data.get('files')[0].get('functions')[0]
-    for stat in example_func_for_stats.keys():
-        if stat in 'probes' or stat in 'name':
-            continue
-        else:
-            sheet.write(0, func_column_index, stat)
-            func_column_index += 1
+    for header in constant.COLUMN_INDEXES.keys():
+        sheet.write(0, constant.COLUMN_INDEXES.get(header), header)
+        if header == 'cd_cov_d_cov' or header == 'cd_uncov_d_uncov':
+            sheet.set_column(constant.COLUMN_INDEXES.get(header), 30)
 
     func_column_index = name_func_column + 1
 
@@ -57,20 +65,9 @@ def fill_sheet(raw_data, sheet):
             if len(function_name) > function_name_column_width:
                 function_name_column_width = len(function_name)
 
-            for attrName in func.keys():
-                if attrName in 'probes':
-                    continue
-
-                if attrName in 'name':
-                    sheet.write(func_row, name_func_column, func.get(attrName))
-                else:
-                    # percentage values
-                    converted_value = map_to_percents(func, attrName)
-                    if converted_value is not None:
-                        sheet.write(func_row, func_column_index, "{}%".format(converted_value))
-                    else:
-                        sheet.write(func_row, func_column_index, func.get(attrName))
-                    func_column_index += 1
+            for attrName in constant.COLUMN_INDEXES.keys():
+                strategy(sheet, func_row, func, attrName)
+                func_column_index += 1
 
             func_row += 1
             func_column_index = name_func_column + 1
@@ -85,64 +82,37 @@ def fill_sheet(raw_data, sheet):
 def overall(data, workbook):
     overall_sheet = workbook.add_worksheet('Overall')
     name_column = "A:A"
-    name_column_index = 0
-    example_file_for_stats = data[0].get('files')[0].get('info')
-    header_file_column = 1
-    for stat_key in example_file_for_stats.keys():
-        if stat_key in XML_ATTRIBUTES.get('MTIME'):
-            continue
-        else:
-            if stat_key in XML_ATTRIBUTES.get('NAME'):
-                overall_sheet.write(0, name_column_index, stat_key)
 
-            else:
-                overall_sheet.write(0, header_file_column, stat_key)
-                header_file_column += 1
+    for header in constant.COLUMN_INDEXES.keys():
+        overall_sheet.write(0, constant.COLUMN_INDEXES.get(header) - 1, header)
+        if header == 'cd_cov_d_cov' or header == 'cd_uncov_d_uncov':
+            overall_sheet.set_column(constant.COLUMN_INDEXES.get(header) - 1, 30)
 
     row = 1
-    col = 1
     filename_field_len = 0
     for folder in data:
         for file in folder.get('files'):
             file_info = file.get('info')
-            filename = file.get('info').get('name')
+            filename = file_info.get('name')
             if len(filename) > filename_field_len:
                 filename_field_len = len(filename)
 
             if filename.endswith('.h'):
                 continue
 
-            for attrName in file_info.keys():
+            for attrName in constant.COLUMN_INDEXES.keys():
+                strategy(overall_sheet, row, file_info, attrName, True)
 
-                if attrName in XML_ATTRIBUTES.get('MTIME'):
-                    continue
-                else:
-                    if attrName in XML_ATTRIBUTES.get('NAME'):
-                        overall_sheet.write(row, name_column_index, file_info.get(attrName))
-                    else:
-                        converted_value = map_to_percents(file_info, attrName)
-                        if converted_value is not None:
-                            overall_sheet.write(row, col, "{} %".format(converted_value))
-                        else:
-                            overall_sheet.write(row, col, file_info.get(attrName))
-                        col += 1
             row += 1
-            col = 1
 
     overall_sheet.set_column(name_column, filename_field_len)
 
 
-def map_to_percents(func_or_file, attr):
-    if attr == XML_ATTRIBUTES.get('FUNCTIONS_COVERED'):
-        result = calc_result(func_or_file.get(attr), func_or_file.get(XML_ATTRIBUTES.get('FUNCTIONS_TOTAL')))
-    elif attr == XML_ATTRIBUTES.get('CONDITIONS_COVERED'):
-        result = calc_result(func_or_file.get(attr), func_or_file.get(XML_ATTRIBUTES.get('CONDITIONS_TOTAL')))
-    elif attr == XML_ATTRIBUTES.get('DECISIONS_COVERED'):
-        result = calc_result(func_or_file.get(attr), func_or_file.get(XML_ATTRIBUTES.get('DECISIONS_TOTAL')))
+def percentize(raw_result, attr):
+    if attr in constant.FIELDS_TO_PERCENTIZE:
+        return "{} %".format(raw_result)
     else:
-        result = None
-
-    return result
+        return raw_result
 
 
 def calc_result(a, b):
@@ -150,8 +120,66 @@ def calc_result(a, b):
     floated_b = float(b)
 
     try:
-        result = round(Decimal((floated_a / floated_b) * 100), 1)
+        result = round(Decimal((floated_a / floated_b) * 100))
     except ZeroDivisionError:
         result = 0
 
     return result
+
+
+def strategy(sheet, row, func_or_file_info, attr, is_file=False):
+
+    if attr in constant.EXCLUDE_FIELDS:
+        return
+
+    if attr == 'fn_cov':
+        result = covered_functions(func_or_file_info)
+    elif attr == 'fn_uncov':
+        result = uncovered_functions(func_or_file_info)
+    elif attr == 'cd_cov_d_cov':
+        result = covered_cd_and_d(func_or_file_info)
+    elif attr == 'cd_uncov_d_uncov':
+        result = uncovered_cd_and_d(func_or_file_info)
+    else:
+        result = func_or_file_info.get(attr)
+
+    formatted_result = percentize(result, attr)
+    if is_file:
+        column_index = constant.COLUMN_INDEXES.get(attr) - 1
+    else:
+        column_index = constant.COLUMN_INDEXES.get(attr)
+
+    sheet.write(row, column_index, formatted_result)
+
+
+def covered_functions(func_or_file):
+    total_functions = int(func_or_file.get(constant.XML_ATTRIBUTES.get('FUNCTIONS_TOTAL')))
+    covered_funcs = int(func_or_file.get(constant.XML_ATTRIBUTES.get('FUNCTIONS_COVERED')))
+    return calc_result(covered_funcs, total_functions)
+
+
+def uncovered_functions(func_or_file):
+    total_functions = int(func_or_file.get(constant.XML_ATTRIBUTES.get('FUNCTIONS_TOTAL')))
+    covered_funcs = int(func_or_file.get(constant.XML_ATTRIBUTES.get('FUNCTIONS_COVERED')))
+    return total_functions - covered_funcs
+
+
+def get_sum_of_conditions_and_decisions(func_or_file):
+    conditions_total = int(func_or_file.get(constant.XML_ATTRIBUTES.get('CONDITIONS_TOTAL')))
+    decisions_total = int(func_or_file.get(constant.XML_ATTRIBUTES.get('DECISIONS_TOTAL')))
+    return conditions_total + decisions_total
+
+
+def get_sum_of_covered_conditions_and_decisions(func_or_file):
+    covered_conditions = int(func_or_file.get(constant.XML_ATTRIBUTES.get('CONDITIONS_COVERED')))
+    covered_decisions = int(func_or_file.get(constant.XML_ATTRIBUTES.get('DECISIONS_COVERED')))
+    return covered_conditions + covered_decisions
+
+
+def covered_cd_and_d(func_or_file):
+    return calc_result(get_sum_of_covered_conditions_and_decisions(func_or_file),
+                       get_sum_of_conditions_and_decisions(func_or_file))
+
+
+def uncovered_cd_and_d(func_or_file):
+    return get_sum_of_conditions_and_decisions(func_or_file) - get_sum_of_covered_conditions_and_decisions(func_or_file)
